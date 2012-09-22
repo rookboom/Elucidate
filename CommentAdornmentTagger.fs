27c6937e08cf81a416d882b3ada684418a2b6229
Johan Verwey <johan.verwey@gmail.com>
2012-09-22 11:12:14 -0400

    First commit

---
+++ CommentAdornmentTagger.fs
@@ -0,0 +1,61 @@
+﻿namespace Elucidate
+
+open System
+open System.ComponentModel.Composition
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Tagging
+open Microsoft.VisualStudio.Utilities
+
+type CommentAdornment(view:IWpfTextView) =
+    let layer = view.GetAdornmentLayer("Elucidate")
+    let layoutChanged args = ()
+    do view.LayoutChanged.Add(layoutChanged)
+    member m.View = view
+
+type CommentAdornmentTagger(view:ITextView(*, commentTagger*)) =
+    let tagsChanged = new Event<EventHandler<SnapshotSpanEventArgs>, SnapshotSpanEventArgs>()
+    let handleBufferChanged (sender:obj) (args:TextContentChangedEventArgs) = ()
+    let handleLayoutChanged (sender:obj) (args:TextViewLayoutChangedEventArgs) = ()
+    do view.LayoutChanged.AddHandler(fun sender args -> handleLayoutChanged sender args)
+    do view.TextBuffer.Changed.AddHandler(fun sender args -> handleBufferChanged sender args)
+
+    interface ITagger<IntraTextAdornmentTag> with
+        [<CLIEvent>]
+        member m.TagsChanged = tagsChanged.Publish
+        member m.GetTags(spans) = Seq.empty
+
+(* This export will expose our Comment Adornment Tagger Provider to Visual Studio *)
+[<Export(typeof<IViewTaggerProvider>)>]
+[<ContentType("text")>]
+//[<ContentType("projection")>]
+[<TagType(typeof<IntraTextAdornmentTag>)>]
+type CommentAdornmentTaggerProvider() =
+    [<Import>]
+    let mutable BufferTagAggregatorFactoryService : IBufferTagAggregatorFactoryService = null
+    
+    interface IViewTaggerProvider with
+            
+        member m.CreateTagger<'T when 'T :> ITag>(textView:ITextView, buffer) : ITagger<'T>  = 
+            let create() =
+                let aggregator = BufferTagAggregatorFactoryService.CreateTagAggregator<CommentTag>(buffer)
+                                |> box
+                CommentAdornmentTagger(textView)
+            try
+                downcast(textView.Properties.GetOrCreateSingletonProperty<CommentAdornmentTagger>(
+                                                 fun () -> create())|> box)
+            with
+            | e -> failwith e.Message
+
+[<Export(typeof<IWpfTextViewCreationListener>)>]
+[<ContentType("text")>]
+[<TextViewRole(PredefinedTextViewRoles.Document)>]
+type CommentAdornmentFactory() =
+    [<Export(typeof<AdornmentLayerDefinition>)>]
+    [<Name("Elucidate")>]
+    [<Order(After = PredefinedAdornmentLayers.Selection, Before = PredefinedAdornmentLayers.Text)>]
+    member m.AdornmentLayerDefinition editorAdornmentLayer = null;
+
+    interface IWpfTextViewCreationListener with
+        member m.TextViewCreated(textView) = 
+            CommentAdornment(textView) |> ignore
---
+++ CommentTagger.fs
@@ -0,0 +1,62 @@
+﻿namespace Elucidate
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Text.RegularExpressions
+open System.ComponentModel.Composition
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Tagging
+open Microsoft.VisualStudio.Utilities
+
+(*We need a container class that can hold the comments that are identified in the text.
+This class has to implement the empty interface ITag just to be identified as a tag.*)
+type CommentTag(comment:string) =
+    interface ITag
+    member m.Value = comment
+    
+(* We need a way to identify comments in our code. The CommentTagger will receive a text buffer
+as input and will implement the ITagger interface which will provide a sequence of CommentTag
+objects, each containing it's comment text. *)
+type CommentTagger(buffer:ITextBuffer) as m =
+    let matchExpressions = [|Regex(@"\(\*(?'xaml'(.|[\r\n])*?)\*\)", RegexOptions.Singleline)|]
+    let tagsChanged = new Event<EventHandler<SnapshotSpanEventArgs>, SnapshotSpanEventArgs>()
+
+    let handleBufferChanged (sender:obj) (args:TextContentChangedEventArgs) =
+        if (args.Changes.Count > 0) then
+            let snapshot = args.After
+            let startPos = args.Changes.[0].NewPosition
+            let endPos = args.Changes.[args.Changes.Count - 1].NewEnd
+
+            let totalAffectedSpan = SnapshotSpan(
+                                        snapshot.GetLineFromPosition(startPos).Start,
+                                        snapshot.GetLineFromPosition(endPos).End)
+            tagsChanged.Trigger(m, SnapshotSpanEventArgs(totalAffectedSpan))
+
+    do buffer.Changed.AddHandler(fun sender args -> handleBufferChanged sender args)
+    interface ITagger<CommentTag> with
+        [<CLIEvent>]
+        member m.TagsChanged = tagsChanged.Publish
+        member m.GetTags(spans) =
+            let documentSpan = spans.FirstOrDefault()
+            let snapshot = documentSpan.Snapshot
+            let text =  snapshot.GetText()
+            let tagSpans(regex:Regex)= 
+                let tagSpan(m:Match) = 
+                    let tag =  CommentTag(m.Groups.["xaml"].Value) :> ITag
+                    let span = SnapshotSpan(snapshot, m.Index, m.Length)
+                    TagSpan(span, tag)
+                regex.Matches(text).Cast<Match>()
+                |> Seq.map tagSpan
+                |> Seq.cast
+            matchExpressions
+            |> Seq.collect tagSpans
+            
+(* This export will expose our Comment Tagger Provider to Visual Studio *)
+[<Export(typeof<ITaggerProvider>)>]
+[<ContentType("text")>]
+[<TagType(typeof<CommentTag>)>]
+type CommentTaggerProvider() =
+    interface ITaggerProvider with
+        member m.CreateTagger<'T when 'T :> ITag>(buffer : ITextBuffer) : ITagger<'T> =
+            downcast(buffer.Properties.GetOrCreateSingletonProperty<CommentTagger>(fun () -> CommentTagger(buffer)) |> box)
